448fd74c9deca7fdf596632d285c4668bfbc4c2c
koon1714 <60070080@kmitl.ac.th>
2018-04-18 22:26:07 +0700

    connecting to keypad

---
+++ Arduino door lock/Code/Code.ino
@@ -0,0 +1,16 @@
+#include <Keypad.h> 
+
+const byte rows = 4;
+const byte columns = 4;
+
+char hexaKeys[rows][columns] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+byte row_pins[rows] = {9, 8, 7, 6};
+byte column_pins[columns] = {5, 4, 3, 2};
+
+Keypad keypad_key = Keypad( makeKeymap(hexaKeys), row_pins, column_pins, rows, columns);
