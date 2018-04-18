9608603cf2d513181a15a7da48ee18e9eec96ff1
koon1714 <60070080@kmitl.ac.th>
2018-04-19 01:35:32 +0700

    adding check password

--- Arduino door lock/Code/Code.ino
+++ Arduino door lock/Code/Code.ino
@@ -5,6 +5,8 @@
 #define servo_lock 1
 #define servo_unlock 180
 
+int check = 0;
+
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 Servo servo;
 
@@ -38,3 +40,59 @@ void setup(){
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Enter Password");
+
+void loop(){
+  lock();
+  input_key = keypad1.getKey();
+  if(input_key == '#'){
+    change_password();
+  }
+  if(input_key) {
+    password[i++] = input_key;
+    lcd.setCursor(i, 1);
+    lcd.print("*");
+  }
+  if(i == 6){
+    for(int i = 0; i < 6; i++){
+      present_password[i] = EEPROM.read(i);
+    }
+    if(strcmp(password, present_password) == 0){
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Pass Accepted");
+      unlock();
+      delay(7000);
+      check = 1;
+      i = 0;
+    }
+    else if(check = 0){
+      if(strcmp(password, factory_password) == 0){
+        change_password();
+      }
+    }
+    else{
+      lcd.clear();
+      lcd.print("Wrong Password");
+      delay(2000);
+      lcd.clear();
+      lcd.print("Enter Password");
+      lcd.setCursor(0, 1);
+      i = 0;
+    }
+  }
+    
+  }
+}
+ 
+void check_password(){
+  
+}
+
+
+void lock(){
+  servo.write(servo_lock);
+}
+
+void unlock(){
+  servo.write(servo_unlock);
+}
