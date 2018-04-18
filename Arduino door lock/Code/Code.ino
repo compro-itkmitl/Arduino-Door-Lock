2c62fdf69362ed87de63c9695fe2494d713bd441
koon1714 <60070080@kmitl.ac.th>
2018-04-18 22:32:59 +0700

    setup lcd

--- Arduino door lock/Code/Code.ino
+++ Arduino door lock/Code/Code.ino
@@ -15,3 +15,19 @@ byte row_pins[rows] = {9, 8, 7, 6};
 byte column_pins[columns] = {5, 4, 3, 2};
 
 Keypad keypad_key = Keypad( makeKeymap(hexaKeys), row_pins, column_pins, rows, columns);
+
+void setup(){
+  Serial.begin(9600);
+  servo.attach(13);
+  lcd.begin();
+  lcd.setCursor(0, 0);
+  lcd.print("    Welcome");
+  delay(1500);
+  lcd.setCursor(0, 0);
+  lcd.print("  Arduino Door");
+  lcd.setCursor(0, 1);
+  lcd.print("  Lock Project");
+  delay(1700);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Enter Password");
